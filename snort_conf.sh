c6ab5fd7bf57ef5e98c90aa7cd88f2d6bde6767c
Aleksii <alexmisnik91@gmail.com>
2017-11-06 13:34:01 +0200

    Delete snort_conf.sh

--- snort_conf.sh
+++
@@ -1,6 +0,0 @@
-	sed -i "s/include \$RULE\_PATH/#include \$RULE\_PATH/" /etc/snort/snort.conf
-	snort -T  -c /etc/snort/snort.conf
-	echo "alert icmp any any -> $HOME_NET any (msg:"ICMP test detected"; GID:1; sid:10000001; rev:001; classtype:icmp-event;)" > /etc/snort/rules/local.rules
-	echo "1 || 10000001 || 001 || icmp-event || 0 || ICMP Test detected || url,tools.ietf.org/html/rfc792" > /etc/snort/sid-msg.map
-	/usr/local/bin/snort -T -i eth0 -c /etc/snort/snort.conf
-				
